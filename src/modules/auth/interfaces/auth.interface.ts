536769c6f3bff0506c404e78ac737a4f321905ac
rafittu <rafittu@outlook.com>
2024-02-05 18:35:26 -0300

    label: jwt strategy interface

--- src/modules/auth/interfaces/auth.interface.ts
+++ src/modules/auth/interfaces/auth.interface.ts
@@ -1,3 +1,17 @@
 export interface IUserToken {
   accessToken: string;
 }
+
+export interface IJtwPayload {
+  sub: string;
+  username: string;
+  email: string;
+  iat?: number;
+  exp?: number;
+}
+
+export interface IUserFromJwt {
+  almaId: string;
+  username: string;
+  email: string;
+}
