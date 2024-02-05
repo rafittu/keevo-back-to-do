042ef6658f342cf295da3e1aeaca95397b5263a7
rafittu <rafittu@outlook.com>
2024-02-05 16:09:03 -0300

    label: created user from external api

--- src/modules/user/interfaces/repository.interface.ts
+++ src/modules/user/interfaces/repository.interface.ts
@@ -3,3 +3,31 @@ import { CreateUserDtoWithChannel } from './user.interface';
 export interface IUserRepository {
   createUser(data: CreateUserDtoWithChannel);
 }
+
+export interface IAlmaUser {
+  id: string;
+  personal: {
+    id: string;
+    firstName: string;
+    lastName: string;
+    socialName?: string;
+    bornDate: string;
+    motherName: string;
+    updatedAt?: Date;
+  };
+  contact: {
+    id: string;
+    username?: string;
+    email: string;
+    phone: string;
+    updatedAt?: Date;
+  };
+  security: {
+    id: string;
+    status: string;
+    updatedAt?: Date;
+  };
+  allowedChannels: string[];
+  createdAt: Date;
+  updatedAt: Date;
+}
