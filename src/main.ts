61c34281a1f3ca1c30dc70117e31467593c1e961
rafittu <rafittu@outlook.com>
2024-02-05 13:25:09 -0300

    chore: set up helmet for server security

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.use(helmet.hidePoweredBy());
+  app.use(helmet.contentSecurityPolicy());
 
   app.enableShutdownHooks();
 
