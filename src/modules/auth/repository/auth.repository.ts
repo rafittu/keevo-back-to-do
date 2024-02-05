932352d02984b9e6d1097b17a087ebd865bd4b36
rafittu <rafittu@outlook.com>
2024-02-05 17:51:34 -0300

    feat: repository function to signin

--- src/modules/auth/repository/auth.repository.ts
+++ src/modules/auth/repository/auth.repository.ts
@@ -3,6 +3,8 @@ import { PrismaService } from '../../../prisma.service';
 import axios from 'axios';
 import { IAuthRepository } from '../interfaces/repository.interface';
 import { AppError } from '../../../common/errors/Error';
+import { CredentialsDto } from '../dto/user-credentials.dto';
+import { IJwtToken } from '../interfaces/auth.interface';
 
 @Injectable()
 export class AuthRepository implements IAuthRepository {
@@ -15,6 +17,31 @@ export class AuthRepository implements IAuthRepository {
     } catch (error) {
       const { status, code, message } = error.response?.data?.error || {};
       throw new AppError(status, code, message);
+    }
+  }
+
+  async signIn(credentials: CredentialsDto): Promise<IJwtToken> {
+    const signInPath: string = process.env.SIGNIN_PATH;
+
+    try {
+      const { accessToken } = await this.almaRequest<IJwtToken>(
+        signInPath,
+        credentials,
+      );
+
+      return { accessToken };
+    } catch (error) {
+      const { status, message } = error || {};
+
+      if (error instanceof AppError) {
+        throw new AppError(status, 400, message);
+      }
+
+      throw new AppError(
+        'auth-repository.signIn',
+        500,
+        'internal server error',
+      );
     }
   }
 }
