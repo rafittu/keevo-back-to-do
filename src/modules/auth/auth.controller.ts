182ea91c8c2c43d26d3931aef31b6be91318a05a
rafittu <rafittu@outlook.com>
2024-02-05 17:43:06 -0300

    feat: signin controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,42 +1,17 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
-import { AuthService } from './services/auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Controller, Post, Body, UseFilters } from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/common/filter/http-exception.filter';
+import { AppError } from 'src/common/errors/Error';
+import { SignInService } from './services/signin.service';
+import { CredentialsDto } from './dto/user-credentials.dto';
+import { IJwtToken } from './interfaces/auth.interface';
 
+@UseFilters(new HttpExceptionFilter(new AppError()))
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly signInService: SignInService) {}
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  @Post('/signin')
+  signIn(@Body() credentials: CredentialsDto): Promise<IJwtToken> {
+    return this.signInService.execute(credentials);
   }
 }
