b00ed5367513c646d6d21daab2d0ec8ac057108e
rafittu <rafittu@outlook.com>
2024-02-05 17:45:23 -0300

    feat: signin service

---
+++ src/modules/auth/services/signin.service.ts
@@ -0,0 +1,17 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { AuthRepository } from '../repository/auth.repository';
+import { IAuthRepository } from '../interfaces/repository.interface';
+import { CredentialsDto } from '../dto/user-credentials.dto';
+import { IJwtToken } from '../interfaces/auth.interface';
+
+@Injectable()
+export class SignInService {
+  constructor(
+    @Inject(AuthRepository)
+    private authRepository: IAuthRepository,
+  ) {}
+
+  execute(credentials: CredentialsDto): Promise<IJwtToken> {
+    return this.authRepository.signIn(credentials);
+  }
+}
