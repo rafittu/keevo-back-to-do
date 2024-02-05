b5c2566b7629e21289fb937e5ca83c76cc4ca915
rafittu <rafittu@outlook.com>
2024-02-05 15:16:00 -0300

    feat: create user controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,15 +1,31 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseFilters,
+} from '@nestjs/common';
 import { UserService } from './services/user.service';
+import { CreateUserService } from './services/create-user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { HttpExceptionFilter } from 'src/common/filter/http-exception.filter';
+import { AppError } from 'src/common/errors/Error';
 
+@UseFilters(new HttpExceptionFilter(new AppError()))
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly createUserService: CreateUserService,
+  ) {}
 
-  @Post()
+  @Post('/create')
   create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+    return this.createUserService.execute(createUserDto);
   }
 
   @Get()
