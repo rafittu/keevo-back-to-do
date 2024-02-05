a5a723d3b30c80be4fc71178c3afb0883710d1ee
rafittu <rafittu@outlook.com>
2024-02-05 18:40:38 -0300

    feat: define public routes

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,12 +4,14 @@ import { AppError } from 'src/common/errors/Error';
 import { SignInService } from './services/signin.service';
 import { CredentialsDto } from './dto/user-credentials.dto';
 import { IUserToken } from './interfaces/auth.interface';
+import { isPublic } from './infra/decorators/is-public.decorator';
 
 @UseFilters(new HttpExceptionFilter(new AppError()))
 @Controller('auth')
 export class AuthController {
   constructor(private readonly signInService: SignInService) {}
 
+  @isPublic()
   @Post('/signin')
   signIn(@Body() credentials: CredentialsDto): Promise<IUserToken> {
     return this.signInService.execute(credentials);
