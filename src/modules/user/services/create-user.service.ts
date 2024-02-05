8a17f13062cd5b10e17f2431c4548a3c0f6c6a6b
rafittu <rafittu@outlook.com>
2024-02-05 15:40:19 -0300

    feat: create user service

---
+++ src/modules/user/services/create-user.service.ts
@@ -0,0 +1,31 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { UserRepository } from '../repository/user.repository';
+import { IUserRepository } from '../interfaces/repository.interface';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { AppError } from '../../../common/errors/Error';
+
+@Injectable()
+export class CreateUserService {
+  constructor(
+    @Inject(UserRepository)
+    private userRepository: IUserRepository,
+  ) {}
+
+  async execute(userData: CreateUserDto) {
+    try {
+      const user = await this.userRepository.createUser(userData);
+
+      return user;
+    } catch (error) {
+      if (error instanceof AppError) {
+        throw error;
+      }
+
+      throw new AppError(
+        'user-service.createUser',
+        500,
+        'failed to create user',
+      );
+    }
+  }
+}
