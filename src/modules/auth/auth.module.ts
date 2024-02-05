df2e7376173aaa2980b2c209eaa7dcfede381da0
rafittu <rafittu@outlook.com>
2024-02-05 17:53:01 -0300

    chore: update auth module providers

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { AuthRepository } from './repository/auth.repository';
+import { SignInService } from './services/signin.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [],
+  providers: [AuthRepository, SignInService],
 })
 export class AuthModule {}
