81fca0827d89b6ee40c8636e37eabf7e7bf63949
rafittu <rafittu@outlook.com>
2024-02-06 10:40:32 -0300

    chore: add new secret environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,6 +19,7 @@ import { JwtAuthGuard } from './modules/auth/infra/guards/jwt-auth.guard';
         POSTGRES_HOST_CONTAINER: Joi.string().required(),
         SIGNUP_PATH: Joi.string().required(),
         SIGNIN_PATH: Joi.string().required(),
+        GET_USER_PATH: Joi.string().required(),
         JWT_SECRET: Joi.string().required(),
       }),
     }),
