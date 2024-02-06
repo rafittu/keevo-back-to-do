b8ce4682b276005bd84d4f3fc97b6eb72b1a2188
rafittu <rafittu@outlook.com>
2024-02-06 16:07:33 -0300

    chore: set up services unit tests

--- src/modules/user/test/user.service.spec.ts
+++ src/modules/user/test/user.service.spec.ts
@@ -1,18 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UserService } from '../services/user.service';
+import { CreateUserService } from '../services/create-user.service';
+import { FindUserService } from '../services/find-user.service';
+import { UpdateUserService } from '../services/update-user.service';
+import { DeleteUserService } from '../services/delete-user.service';
+import { UserRepository } from '../repository/user.repository';
 
-describe('UserService', () => {
-  let service: UserService;
+describe('User Services', () => {
+  let createUserService: CreateUserService;
+  let findUserService: FindUserService;
+  let updateUserService: UpdateUserService;
+  let deleteUserService: DeleteUserService;
+
+  let userRepository: UserRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        CreateUserService,
+        FindUserService,
+        UpdateUserService,
+        DeleteUserService,
+        {
+          provide: UserRepository,
+          useValue: {
+            createUser: jest.fn().mockResolvedValue(''),
+            findById: jest.fn().mockResolvedValue(''),
+            updateUser: jest.fn().mockResolvedValue(''),
+            deleteUser: jest.fn().mockResolvedValue(''),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    createUserService = module.get<CreateUserService>(CreateUserService);
+    findUserService = module.get<FindUserService>(FindUserService);
+    updateUserService = module.get<UpdateUserService>(UpdateUserService);
+    deleteUserService = module.get<DeleteUserService>(DeleteUserService);
+
+    userRepository = module.get<UserRepository>(UserRepository);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(createUserService).toBeDefined();
+    expect(findUserService).toBeDefined();
+    expect(updateUserService).toBeDefined();
+    expect(deleteUserService).toBeDefined();
   });
 });
