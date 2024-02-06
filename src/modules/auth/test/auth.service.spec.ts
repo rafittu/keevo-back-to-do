faaee005a114077ef7e888dff08b0f90252c3f29
rafittu <rafittu@outlook.com>
2024-02-06 18:19:32 -0300

    test: user sign in service

--- src/modules/auth/test/auth.service.spec.ts
+++ src/modules/auth/test/auth.service.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from '../services/auth.service';
+import { SignInService } from '../services/signin.service';
+import { AuthRepository } from '../repository/auth.repository';
+import { MockAccessToken, MockUserCredentials } from './mocks/auth.mock';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let signInService: SignInService;
+
+  let authRepository: AuthRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        SignInService,
+        {
+          provide: AuthRepository,
+          useValue: {
+            signIn: jest.fn().mockResolvedValue(MockAccessToken),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    signInService = module.get<SignInService>(SignInService);
+
+    authRepository = module.get<AuthRepository>(AuthRepository);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(signInService).toBeDefined();
+  });
+
+  describe('user sign in', () => {
+    it('user should sign in successfully', async () => {
+      const result = await signInService.execute(MockUserCredentials);
+
+      expect(authRepository.signIn).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(MockAccessToken);
+    });
   });
 });
