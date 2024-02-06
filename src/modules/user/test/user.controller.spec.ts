fac1024d9ad7b754c53c846423444a8ba0563516
rafittu <rafittu@outlook.com>
2024-02-06 15:37:13 -0300

    chore: set up controller unit tests

--- src/modules/user/test/user.controller.spec.ts
+++ src/modules/user/test/user.controller.spec.ts
@@ -1,17 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from '../user.controller';
-import { UserService } from '../services/user.service';
+import { CreateUserService } from '../services/create-user.service';
+import { FindUserService } from '../services/find-user.service';
+import { UpdateUserService } from '../services/update-user.service';
+import { DeleteUserService } from '../services/delete-user.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  let createUserService: CreateUserService;
+  let findUserService: FindUserService;
+  let updateUserService: UpdateUserService;
+  let deleteUserService: DeleteUserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService],
+      providers: [
+        {
+          provide: CreateUserService,
+          useValue: {
+            execute: jest.fn().mockResolvedValue(''),
+          },
+        },
+        {
+          provide: FindUserService,
+          useValue: {
+            execute: jest.fn().mockResolvedValue(''),
+          },
+        },
+        {
+          provide: UpdateUserService,
+          useValue: {
+            execute: jest.fn().mockResolvedValue(''),
+          },
+        },
+        {
+          provide: DeleteUserService,
+          useValue: {
+            execute: jest.fn().mockResolvedValue(''),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    createUserService = module.get<CreateUserService>(CreateUserService);
+    findUserService = module.get<FindUserService>(FindUserService);
+    updateUserService = module.get<UpdateUserService>(UpdateUserService);
+    deleteUserService = module.get<DeleteUserService>(DeleteUserService);
   });
 
   it('should be defined', () => {
