c209cf2130c265f44b78978812a0c768d75373de
rafittu <rafittu@outlook.com>
2024-02-07 14:11:12 -0300

    chore: update task module providers list

--- src/modules/task/task.module.ts
+++ src/modules/task/task.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
+import { PrismaService } from '../../prisma.service';
+import { TaskRepository } from './repository/task.repository';
+import { CreateTaskService } from './services/create-task.service';
 
 @Module({
   controllers: [TaskController],
-  providers: [],
+  providers: [PrismaService, TaskRepository, CreateTaskService],
 })
 export class TaskModule {}
