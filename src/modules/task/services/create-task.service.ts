5006771bb7dc164261b1221f884f1ec4040bf44f
rafittu <rafittu@outlook.com>
2024-02-07 12:40:49 -0300

    feat: create task service

---
+++ src/modules/task/services/create-task.service.ts
@@ -0,0 +1,32 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { TaskRepository } from '../repository/task.repository';
+import { ITaskRepository } from '../interfaces/repository.interface';
+import { AppError } from '../../../common/errors/Error';
+import { IUserFromJwt } from '../../../modules/auth/interfaces/auth.interface';
+import { CreateTaskDto } from '../dto/create-task.dto';
+
+@Injectable()
+export class CreateTaskService {
+  constructor(
+    @Inject(TaskRepository)
+    private taskRepository: ITaskRepository,
+  ) {}
+
+  async execute(user: IUserFromJwt, task: CreateTaskDto) {
+    const { almaId } = user;
+
+    try {
+      return await this.taskRepository.createTask(almaId, task);
+    } catch (error) {
+      if (error instanceof AppError) {
+        throw error;
+      }
+
+      throw new AppError(
+        'task-service.createTask',
+        500,
+        'failed to create task',
+      );
+    }
+  }
+}
