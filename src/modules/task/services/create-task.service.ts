28485ee0d0cef0d7ef0f54a393d5a8c942966df2
rafittu <rafittu@outlook.com>
2024-02-07 13:47:34 -0300

    art: improve service structure

--- src/modules/task/services/create-task.service.ts
+++ src/modules/task/services/create-task.service.ts
@@ -4,6 +4,7 @@ import { ITaskRepository } from '../interfaces/repository.interface';
 import { AppError } from '../../../common/errors/Error';
 import { IUserFromJwt } from '../../../modules/auth/interfaces/auth.interface';
 import { CreateTaskDto } from '../dto/create-task.dto';
+import { TaskStatus } from '@prisma/client';
 
 @Injectable()
 export class CreateTaskService {
@@ -16,7 +17,11 @@ export class CreateTaskService {
     const { almaId } = user;
 
     try {
-      return await this.taskRepository.createTask(almaId, task);
+      return await this.taskRepository.createTask(
+        almaId,
+        task,
+        TaskStatus.TODO,
+      );
     } catch (error) {
       if (error instanceof AppError) {
         throw error;
