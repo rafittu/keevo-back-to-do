44962c47d68fc8b8fe0940bb8ba80bb1e39fe1f6
rafittu <rafittu@outlook.com>
2024-02-10 16:09:36 -0300

    fix: create user dueDate validation

--- src/modules/task/dto/create-task.dto.ts
+++ src/modules/task/dto/create-task.dto.ts
@@ -4,13 +4,9 @@ import {
   IsNotEmpty,
   IsOptional,
   IsString,
-  Matches,
   MaxLength,
   MinLength,
 } from 'class-validator';
-
-const dateRegexPattern =
-  '^([0-9]{4}-((0[13578]|1[02])-([0-2][1-9]|3[0-1])|(0[469]|11)-(0[1-9]|[1-2][0-9]|30)|02-(0[1-9]|[1-2][0-9]))|([0-9]{2}(([2468][048]|[02468][48])|[13579][26])|([13579][26]|[02468][048]|0[0-9]|1[0-6])00)-02-29)$';
 
 export class CreateTaskDto {
   @IsNotEmpty()
@@ -33,9 +29,6 @@ export class CreateTaskDto {
   @IsOptional()
   @IsString()
   @MaxLength(10, { message: 'must be a valid date: yyyy-mm-dd' })
-  @Matches(new RegExp(dateRegexPattern), {
-    message: 'must be a valid date and formatted as yyyy-mm-dd',
-  })
   dueDate?: string;
 
   @IsOptional()
