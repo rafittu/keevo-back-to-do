c810e0ea12f6d79b970f50575bd7003bec4ccf08
rafittu <rafittu@outlook.com>
2024-02-14 17:30:57 -0300

    test: format task responses repository

--- src/modules/task/test/mocks/task.mock.ts
+++ src/modules/task/test/mocks/task.mock.ts
@@ -2,7 +2,7 @@ import { faker } from '@faker-js/faker';
 import { IUserFromJwt } from '../../../../modules/auth/interfaces/auth.interface';
 import { MockAlmaUser } from '../../../../modules/user/test/mocks/user.mock';
 import { CreateTaskDto } from '../../dto/create-task.dto';
-import { TaskPriority, TaskStatus } from '@prisma/client';
+import { Task, TaskPriority, TaskStatus } from '@prisma/client';
 import { ITask, ITaskData } from '../../interfaces/task.interface';
 import { TaskFilterDto } from '../../dto/filter-task.dto';
 import { UpdateTaskDto } from '../../dto/update-task.dto';
@@ -21,15 +21,28 @@ export const MockCreateTask: CreateTaskDto = {
   categories: ['SHOPPING'],
 };
 
-export const MockTask: ITask = {
+export const MockPrismaTask: Task = {
   id: faker.string.uuid(),
+  user_id: faker.string.uuid(),
+  title: MockCreateTask.title,
+  description: MockCreateTask.description,
+  priority: MockCreateTask.priority,
+  due_date: new Date(MockCreateTask.dueDate),
+  status: TaskStatus.TODO,
+  completed_at: null,
+  created_at: faker.date.recent(),
+  updated_at: faker.date.recent(),
+};
+
+export const MockTask: ITask = {
+  id: MockPrismaTask.id,
   title: MockCreateTask.title,
   description: MockCreateTask.description,
   priority: MockCreateTask.priority,
   dueDate: MockCreateTask.dueDate,
   status: TaskStatus.TODO,
   categories: MockCreateTask.categories,
-  createdAt: faker.date.recent(),
+  createdAt: MockPrismaTask.created_at,
 };
 
 export const MockFilterTask: TaskFilterDto = {
@@ -39,17 +52,17 @@ export const MockFilterTask: TaskFilterDto = {
 };
 
 export const MockTaskData: ITaskData = {
-  taskId: MockTask.id,
-  userId: faker.string.uuid(),
-  title: MockTask.title,
-  description: MockTask.description,
-  priority: MockTask.priority,
-  dueDate: new Date(MockTask.dueDate),
+  taskId: MockPrismaTask.id,
+  userId: MockPrismaTask.user_id,
+  title: MockPrismaTask.title,
+  description: MockPrismaTask.description,
+  priority: MockPrismaTask.priority,
+  dueDate: MockPrismaTask.due_date,
   categories: MockTask.categories,
-  status: MockTask.status,
-  completedAt: null,
-  createdAt: MockTask.createdAt,
-  updatedAt: faker.date.recent(),
+  status: MockPrismaTask.status,
+  completedAt: MockPrismaTask.completed_at,
+  createdAt: MockPrismaTask.created_at,
+  updatedAt: MockPrismaTask.updated_at,
 };
 
 export const MockUpdateTask: UpdateTaskDto = {
--- src/modules/task/test/task.repository.spec.ts
+++ src/modules/task/test/task.repository.spec.ts
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { PrismaService } from '../../../prisma.service';
 import { AppError } from '../../../common/errors/Error';
 import { TaskRepository } from '../repository/task.repository';
+import { MockPrismaTask, MockTaskData } from './mocks/task.mock';
 
 jest.mock('axios');
 
@@ -25,5 +26,29 @@ describe('UserRepository', () => {
   it('should be defined', () => {
     expect(taskRepository).toBeDefined();
     expect(prismaService).toBeDefined();
+  });
+
+  describe('format task responses', () => {
+    it('should format one task', async () => {
+      const result = taskRepository['formatTask'](MockPrismaTask);
+
+      const MockResult = {
+        ...result,
+        categories: MockTaskData.categories,
+      };
+
+      expect(MockResult).toEqual(MockTaskData);
+    });
+
+    it('should format many tasks', async () => {
+      jest
+        .spyOn(taskRepository as any, 'formatManyTasks')
+        .mockReturnValueOnce(MockTaskData);
+
+      const result = taskRepository['formatManyTasks']([MockPrismaTask]);
+
+      expect(taskRepository['formatManyTasks']).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(MockTaskData);
+    });
   });
 });
